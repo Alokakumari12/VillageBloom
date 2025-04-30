65a19c769700a54343300d48f28632b3905fb51e
Ravinjith <ravinjith2001rv@gmail.com>
2025-04-30 22:33:17 +0530

    commit-13-ranidu-form

--- backend/Models/SellerManagement/SellerModel.js
+++ backend/Models/SellerManagement/SellerModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const SellerSchema = new Schema({
+const SellerSchema = new Schema({//form
 
     fullName: {
         type: String,
