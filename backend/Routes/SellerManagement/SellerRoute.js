55d5ab75eebfbda078782a005370aba5fb54fcf5
Ravinjith <ravinjith2001rv@gmail.com>
2025-04-30 23:01:31 +0530

    commit-23-ranidu-routepost

--- backend/Routes/SellerManagement/SellerRoute.js
+++ backend/Routes/SellerManagement/SellerRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const SellerController = require("../../Controllers/SellerManagement/SellerController");//seller controller path
 
 router.get("/", SellerController.getAllDetails);
-router.post("/", SellerController.addData);
+router.post("/", SellerController.addData);//post
 router.get("/:id", SellerController.getById);//get
 router.put("/:id", SellerController.updateData);//put
 router.delete("/:id", SellerController.deleteData);//delete
