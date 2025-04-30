c4107222a304194f09101b70e7584ab76c6af86c
Ravinjith <ravinjith2001rv@gmail.com>
2025-04-30 22:42:58 +0530

    commit-19-ranidu-sellerpath

--- backend/Routes/SellerManagement/SellerRoute.js
+++ backend/Routes/SellerManagement/SellerRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const SellerController = require("../../Controllers/SellerManagement/SellerController");
+const SellerController = require("../../Controllers/SellerManagement/SellerController");//seller controller path
 
 router.get("/", SellerController.getAllDetails);
 router.post("/", SellerController.addData);
